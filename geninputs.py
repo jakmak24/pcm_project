0d6033eb2b7c5a2c81eb2fa1ae5a1a658e972b68
maciejspychala <mac.spychala@gmail.com>
2019-01-16 00:39:30 +0100

    generate transactions without blancks

--- geninputs.py
+++ geninputs.py
@@ -11,8 +11,8 @@ def wrap(n):
     else:
         return str(n)
 
-def random_line():
-    return [ wrap(random.randint(0, SPARSE_LEVEL*MAX_VALUE)) for _ in range(10) ]
+def random_line(sparse = SPARSE_LEVEL):
+    return [ wrap(random.randint(0, sparse*MAX_VALUE)) for _ in range(10) ]
     
 def random_class():
     return str(random.randint(0, MAX_CLASS))
@@ -25,18 +25,17 @@ def write_rule(fname, size_ratio=1):
 def write_file(fname, size_ratio=1):
     with open(fname, 'w') as rulef:
         for i in range(int(size_ratio * RULES_LENGTH)):
-            rulef.write(";".join(random_line()) + "\n")
+            rulef.write(";".join(random_line(sparse = 1)) + "\n")
         
         
 if __name__ == '__main__':
     "Real input"
     ratio = 1
     write_rule("rule_2M.csv", ratio)
-    for i in range(0,10):
+    for i in range(0,2):
         write_file("transactions_{}.csv".format(i), ratio)
     
     "Tiny input"
     ratio = .01
     write_rule("rule_tiny.csv", ratio)
     write_file("transactions_tiny.csv", ratio)
-    
